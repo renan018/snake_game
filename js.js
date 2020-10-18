893714ea61b96cf13b42d5616172120545c8529a
Renan Chaparro <renan.boxmail@gmail.com>
2020-10-18 02:26:16 -0300

    Aumenta corpo e passa pelas paredes

--- js.js
+++ js.js
@@ -9,9 +9,6 @@ function getPosFruta(min, max) {
 var sp = document.querySelector("p");
 var sc = document.querySelectorAll("p.titulo_topo_canvas")[1];
 var escutaTecla = document.querySelector("body");
-var fast = document.getElementById("fast");
-var medium = document.getElementById("medium");
-var slowly = document.getElementById("slowly");
 var colorSnake = document.getElementById("colorSnake").value;
 var colorFruit = document.getElementById("colorFruit").value;
 // a,b,c,d = pontos para desenhar snake
@@ -20,6 +17,7 @@ var colorFruit = document.getElementById("colorFruit").value;
 // contador = quantidade de movimentos
 // mover = controle dos movimentos repetitivos (setInterval)
 // f1, f2 = posição aleatória da maçã
+var teclaPressionada = 0;
 var a = 10,
     b = 10,
     c = 10,
@@ -33,13 +31,32 @@ var a = 10,
     numeroFases = 10,
     fase = 1,
     tempo = 0,
-    contInicioDeFase = 4,
+    progressBar = 0,
     f1 = getPosFruta(0, 290),
     f2 = getPosFruta(0, 140),
     pontos = 0,
     totalPontos = 0,
+    qntFrutasComidas = 0,
     pontosPorFase = 6,
-    velocidade = 120;
+    velocidade = 120, 
+    snake = [],
+    snakeTecla = [],
+    larguraCanvas = 290,
+    alturaCanvas = 140,
+    aumentoDeVelocidade = 10,
+    chaveBloqueio = false;
+    snake[qntFrutasComidas] = {"a":10,"b":10};
+    // Objeto Snake
+    snakeTecla[37] = {defineRota: function(){
+        snake[0].a -= ((snake[0].a > 0) ? movimentos : larguraCanvas);
+        snake[0].a=Math.abs(snake[0].a)}};
+    snakeTecla[38] = {defineRota: function(){
+        snake[0].b -= ((snake[0].b > 0) ? movimentos : alturaCanvas);
+        snake[0].b = Math.abs(snake[0].b);}};
+    snakeTecla[39] = {defineRota: function(){
+        snake[0].a += ((snake[0].a < larguraCanvas) ? movimentos : -larguraCanvas);}};
+    snakeTecla[40] = {defineRota: function(){
+        snake[0].b += (snake[0].b < alturaCanvas) ? movimentos : -alturaCanvas;}};
 
 // cria snake e maçã 
 var ca = document.querySelector("canvas").getContext("2d");
@@ -47,189 +64,170 @@ var fruta = document.querySelector("canvas").getContext("2d");
 function pontoInicial() {
     'use strict';
     clearInterval(mover);
-    a = 10;
-    b = 10;
-    c = 10;
-    d = 10;
-    ca.fillStyle = colorSnake;
-    ca.fillRect(a, b, c, d);
+    snake.splice(1,snake.length);
+    snake[0].a = 10;
+    snake[0].b = 10;
+    desenhaSnake();
+    desenhaFruta();
+}
+function desenhaFruta() {
     fruta.fillStyle = colorFruit;
     fruta.fillRect(f1, f2, c, d);
 }
-
-function mensagem(msg = null) {
-    if(msg == null) {
-        sp.innerHTML = "Level " + fase + " wait: " + contInicioDeFase;    
-    } else {
-        sp.innerHTML = msg;
+function desenhaSnake() {
+    ca.fillStyle = colorSnake;
+    for(let i = 0; i <= qntFrutasComidas; i++){
+        ca.fillRect(snake[i].a, snake[i].b, c, d);
     }
-    sc.textContent = "Score: " + (totalPontos * 100);
+}
+
+function mensagem(msg) {
+    'use strict';
+    if(msg !== undefined)
+    sp.innerHTML = msg;
+    sc.innerHTML = "Score: " + (totalPontos * 100);
 }
 
 // limpa toda tela e desenha snake e maçã
 function limpa() {
     'use strict';
     ca.clearRect(0, 0, 300, 150);
-    if (a + 10 >= f1 && a <= f1 + 9 && b <= f2 + 9 && b + 9 >= f2) {
-        teclaBkp = 0;
+}
+function montaCorpo(e){
+    if(!qntFrutasComidas) return;
+    let i = 0;
+    do {
+        var tempx = snake[i+1].a;
+        var tempy = snake[i+1].b;
+        snake[i+1].a = a;
+        snake[i+1].b = b;
+        a = tempx;
+        b = tempy;
+        i += 1;
+    } while (i < qntFrutasComidas);    
+}
+function verificaSeCobraComeuFruta(e) {
+    if (snake[0].a + 10 >= f1 && snake[0].a <= f1 + 9 
+        && snake[0].b <= f2 + 9 && snake[0].b + 9 >= f2) {
         f1 = getPosFruta(0, 290);
         f2 = getPosFruta(0, 140);
         pontos += 1;
-        if (pontos === pontosPorFase) {
-            proximaFase();
-        }
-    } else {
-        fruta.fillStyle = colorFruit;
-        fruta.fillRect(f1, f2, c, d);
+        totalPontos += 1;
+        qntFrutasComidas += 1;   
+        snake[qntFrutasComidas] = {a,b};
+        sc.textContent = "Score: " + (totalPontos * 100);
+        mensagem("&#x1F40D; &#x1F34E;");
     }
 }
-
-
-// Compara teclas e incrementa posição para fazer o movimento
-// linha 91 tem a descrição das teclas
-function movimenta(e) {
+// Movimenta de acordo com tecla digitada
+function movimenta(teclaPressionada) {
     'use strict';
-    if (e === 37 && a > 0) {
-        a -= movimentos;
-    }
-    if (e === 38 && b > -1) {
-        b -= movimentos;
-        if (b < -1) {
-            b = 0;
-        }
-    }
-    if (e === 39 && a < 291) {
-        a += movimentos;
-        if (a > 290) {
-            a = 290;
-        }
-    }
-    if (e === 40 && b < 141) {
-        b += movimentos;
-        if (b > 140) {
-            b = 140;
-        }
-    }
-    if ((a <= 0 && e === 37) ||
-            (a >= 290 && e === 39) ||
-            (b <= 0 && e === 38) ||
-            (b >= 140 && e === 40)) {
-        clearInterval(mover);
-    }
+    a = snake[0].a;
+    b = snake[0].b;
+    snakeTecla[teclaPressionada].defineRota();
     limpa();
-    ca.fillStyle = colorSnake;
-    ca.fillRect(a, b, c, d);
+    verificaSeCobraComeuFruta(teclaPressionada);
+    montaCorpo(teclaPressionada);
+    desenhaSnake();
+    desenhaFruta();
+    chaveBloqueio = false;
 }
 
 // Responsável pelo movimento
-function intervalo(n) {
+function intervalo(teclaPressionada) {
     'use strict';
     mover = setInterval(function () {
-        movimenta(n);
+        movimenta(teclaPressionada);
     }, velocidade);
 }
 
 // Controle
 function acao(event) {
     'use strict';
-    e = event.keyCode;
-    if (teclaBkp !== e) {
-        teclaBkp = e;
-        clearInterval(mover);
-        if (e >= 37 && e <= 40) {
-            intervalo(e);
+    teclaPressionada = event.keyCode;
+    if (teclaBkp !== teclaPressionada) {
+        if((teclaBkp === 37 && teclaPressionada === 39) || (teclaBkp === 39 && teclaPressionada === 37)) return;
+        if((teclaBkp === 38 && teclaPressionada === 40) || (teclaBkp === 40 && teclaPressionada === 38)) return;
+        if(chaveBloqueio === false){
+            teclaBkp = teclaPressionada;
+            clearInterval(mover);
+            if (teclaPressionada >= 37 && teclaPressionada <= 40) {
+                chaveBloqueio = true;
+                intervalo(teclaPressionada);
+            }    
         }
+        
     }
 }
 
-// Início Fase
-function inicioFase() {
+function getPosEmoji(max) {
     'use strict';
-    mensagem();
-    contInicioDeFase -= 1;
-    if (contInicioDeFase === -1) {
-        mensagem("Go!!!!");
-        clearInterval(tempo);
-        contInicioDeFase = 4;
-        iniciarJogo();
-    }
+    var min = 0;
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Função próxima fase
-function proximaFase() {
+function getEmojiVencedor() {
     'use strict';
-    clearInterval(mover);
-    if(fase === numeroFases) {
-        mensagem("YOU WIN");
-        sp.style.color="green";
-        resetarJogo();
-    } else {
-        escutaTecla.removeEventListener("keydown", acao);
-        totalPontos += pontos;
-        pontos = 0;
-        fase += 1;
-        // Reiniciar do ponto inicial
-        pontoInicial();
-        // Aumentar velocidade
-        velocidade -= 10;
-        // Decrementar contador para início da próxima fase
-        tempo = setInterval(inicioFase, 600);    
-    }
+    var rostos = ['&#x1F607;', '&#x1F60D;', '&#x1F60E;',
+                  '&#x1F618;', '&#x1F61C;', '&#x1F942;',
+                  '&#x1F947;', '&#x1F948;', '&#x1F949;'];
+    var pos = getPosEmoji(rostos.length);
+    return rostos[pos];
+}
+function getEmojiPontos() {
+    'use strict';
+    var rostos = ['&#x1F600;', '&#x1F601;', '&#x1F603;', '&#x1F604;',
+                  '&#x1F609;', '&#x1F60A;', '&#x1F60B;', '&#x1F60F;',
+                  '&#x1F617;', '&#x1F61B;', '&#x1F61D;', '&#x1F502;',
+                  '&#x1F504;', '&#x1F51D;', '&#x1F525;', '&#x1F64C;'];
+    var pos = getPosEmoji(rostos.length);
+    return rostos[pos];
 }
 
-function getVelocidade() {
-    if(fast.hasAttribute("data-velocidade")) {
-       return 50;
-    } else if(medium.hasAttribute("data-velocidade")) {
-        return 100;
-    } else if(slowly.hasAttribute("data-velocidade")) {
-        return 400;
-    }
+function escutarTeclado() {
+    escutaTecla.addEventListener("keydown", acao);
+}
+function pararDeEscutarTeclado() {
+    escutaTecla.removeEventListener("keydown", acao);
 }
 
 // Evento que escuta teclas pressionadas
 // Ao clicar chama função acao passando o código da tecla como parâmetro
 function iniciarJogo() {
     'use strict';
-    mensagem("Go!!!");
-    velocidade = getVelocidade();
-    pontosPorFase = parseInt(document.getElementById("pontos").value);
-    numeroFases = parseInt(document.getElementById("fases").value);
-    escutaTecla.addEventListener("keydown", acao);    
+    mensagem("&#x1F449; &#x1F3AE; Move arrow!");
+    escutarTeclado();
     colorSnake = document.getElementById("colorSnake").value;
     colorFruit = document.getElementById("colorFruit").value;
-}
-function resetarJogo() {
-    clearInterval(mover);
-    escutaTecla.removeEventListener("keydown", acao);
+    sc.textContent = "Score: ";
     fase = 0;
     pontos = 0;
-    limpa();
-    pontoInicial();    
-}
-pontoInicial();
-function selecionaBadge(event) {
-    fast.removeAttribute("class, data-velocidade");
-    medium.removeAttribute("class, data-velocidade");
-    slowly.removeAttribute("class, data-velocidade");
-    fast.removeAttribute("data-velocidade");
-    medium.removeAttribute("data-velocidade");
-    slowly.removeAttribute("data-velocidade");
-    fast.setAttribute("class", "badge badge-pill badge-secondary");
-    medium.setAttribute("class", "badge badge-pill badge-secondary");
-    slowly.setAttribute("class", "badge badge-pill badge-secondary");
-    event.target.removeAttribute("class");
-    event.target.setAttribute("class", "badge badge-pill badge-success");
-    event.target.setAttribute("data-velocidade", "1");
-}
-function trocaCor() {
-    colorSnake = document.getElementById("colorSnake").value;
-    colorFruit = document.getElementById("colorFruit").value;
+    velocidade = 120;
+    qntFrutasComidas = 0;
+    totalPontos = 0;
+    teclaBkp = 0;
     limpa();
     pontoInicial();
 }
-fast.addEventListener("click",selecionaBadge);
-medium.addEventListener("click",selecionaBadge);
-slowly.addEventListener("click",selecionaBadge);
+// Inicia programa desenhando snake e fruta
+pontoInicial();
+function trocaCor(e) {
+    'use strict';
+    e.preventDefault();
+    colorSnake = document.getElementById("colorSnake").value;
+    colorFruit = document.getElementById("colorFruit").value;
+}
 // testes em:
 //https://jsfiddle.net/renan_chaparro/xg3r6qjw/
+function getEmojiSad() {
+    'use strict';
+    var rostos = ['&#x1F926;', '&#x1F937;', '&#x1F637;', '&#x1F648;',
+                  '&#x1F649;', '&#x1F602;', '&#x1F605;', '&#x1F606;',
+                  '&#x1F608;', '&#x1F60C;', '&#x1F610;', '&#x1F611;',
+                  '&#x1F612;', '&#x1F613;', '&#x1F614;', '&#x1F615;',
+                  '&#x1F616;', '&#x1F61E;', '&#x1F61F;', '&#x1F620;',
+                  '&#x1F621;'];
+    var pos = getPosEmoji(rostos.length);
+    return rostos[pos];
+}
